75d5a4f71e7a9eeace0205533688091aa1b7a10a
Colin Shelton <sheltoncolint@gmail.com>
2020-09-03 18:30:07 -0400

    writeFileSync intitalized

--- index.js
+++ index.js
@@ -1,120 +1,80 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = inquirer.prompt([
-    // #Title: 
-    {
-        type: "input",
-        name: "title",
-        message: "Title: "
-    },
-    // ## Description: 
-    {
-        type: "input",
-        name: "description",
-        message: "Description: "
-    },
-    // ## Installation: 
-    {
-        type: "input",
-        name: "installation",
-        message: "Installation: "
-    },
-    // ## App Usage: 
-    {
-        type: "input",
-        name: "usage",
-        message: "App Usage: "
-    },
-    // ## Contribution: 
-    {
-        type: "input",
-        name: "contributing",
-        message: "Contributing: "
-    },
-    // // ## License Details: NEEDs badge for license to be added at top  
-    {
-        type: "list",
-        name: "License Details",
-        message: "License Details: ",
-        choices: ['MIT license (MIT)', 'Apache License 2.0 (Apache-2.0)', '3-clause BSD license (BSD-3-Clause)', 'GNU General Public License (GPL)', 'Common Development and Distribution License 1.0 (CDDL-1.0)']
-    },
-    // ## Tests:
-    {
-        type: "input",
-        name: "tests",
-        message: "Tests: "
-    },
-    // // ## Questions: NEEDs 
-    // {
-    //     type: "input",
-    //     name: "Questions",
-    //     message: "Questions: "
-    // }
-])
-    .then(answers => {
-        // use user feedback for whatever 
-    })
-    .catch(error => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else when wrong
-        }
-    });
+const questions = [
+  // #Title:
+  {
+    type: "input",
+    name: "title",
+    message: "Title: ",
+  },
+  // ## Description:
+  {
+    type: "input",
+    name: "description",
+    message: "Description: ",
+  },
+  // ## Installation:
+  {
+    type: "input",
+    name: "installation",
+    message: "Installation: ",
+  },
+  // ## App Usage:
+  {
+    type: "input",
+    name: "usage",
+    message: "App Usage: ",
+  },
+  // ## Contribution:
+  {
+    type: "input",
+    name: "contributing",
+    message: "Contributing: ",
+  },
+  // // ## License Details: NEEDs badge for license to be added at top
+  {
+    type: "list",
+    name: "license",
+    message: "License Details: ",
+    choices: [
+      "MIT license (MIT)",
+      "Apache License 2.0 (Apache-2.0)",
+      "3-clause BSD license (BSD-3-Clause)",
+      "GNU General Public License (GPL)",
+      "Common Development and Distribution License 1.0 (CDDL-1.0)",
+    ],
+  },
+  // ## Tests:
+  {
+    type: "input",
+    name: "tests",
+    message: "Tests: ",
+  },
+];
 
 // function to write README file
-function writeToReadme(answers) {
-    // some if statements depending on user input 
-    return `
-# ${answers.title}
 
-## Description
-
-## Table of Contents
-
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [License](#license)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
-  * [Credits](#credits)
-
-## Installation
-
-${answers.installation}
-
-## Usage
-
-${answers.usage}
-
-## License
-
-## Contributing
-
-${answers.contributing}
-
-## Tests
-
-${answers.tests}
-
-## Questions
-
-If you have questions contact via [Github](https://github.com/colinshelton) or [email](https://mail.google.com/mail/?view=cm&source=mailto&to=[sheltoncolint@gmail.com]). 
-
-`};
-
+function writeToReadme(markDown) {
+  //some if statements depending on user input
+  fs.writeFileSync("./output/README.md", markDown);
+}
 
 // function to initialize program
-// async function init() {
-//     try {
-//         const answers = await prompt();
 
-//     }
-//     return
-// };
+async function init() {
+  try {
+    inquirer.prompt(questions).then(async (answers) => {
+      const markDown = await generateMarkdown(answers);
+      console.log(markDown);
+      writeToReadme(markDown);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 // function call to initialize program
-// init();
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,44 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+function generateMarkdown(answers) {
+  return `# ${answers.title}
+  
+  ## Description
+  ${answers.description}
+  
+  ## Table of Contents
+  
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    * [Credits](#credits)
+  
+  ## Installation
+  
+  ${answers.installation}
+  
+  ## Usage
+  
+  ${answers.usage}
+  
+  ## License
+  
+  ${answers.license}
+  
+  ## Contributing
+  
+  ${answers.contributing}
+  
+  ## Tests
+  
+  ${answers.tests}
+  
+  ## Questions
+  
+  If you have questions contact via [Github](https://github.com/colinshelton) or [email](https://mail.google.com/mail/?view=cm&source=mailto&to=[sheltoncolint@gmail.com]). 
+  
 `;
 }
 
